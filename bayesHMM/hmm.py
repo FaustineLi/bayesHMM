bb49306b05b5639f382ad86d9ee16001d10254ec
Faustine Li <faustineli12@gmail.com>
2018-01-28 21:50:46 -0500

    add unit tests, refactored code for increased readability

--- bayesHMM/hmm.py
+++ bayesHMM/hmm.py
@@ -5,30 +5,32 @@ from tqdm import tqdm
 class HMM(object):
     '''
     Bayesian Hidden Markov Models
-
         n_states: int, number of latent states
-
         n_samps: int, number of sampling iterations
-
         n_burn: int, number of burn-in iterations
-
         prior: dict, set prior parameters
-
         seed: numeric, set random seed
     '''
-
+    
     def __init__(self, n_states, n_samps, n_burn=0, prior=None, seed=None):
+        '''Intializes HMM object '''
+        
+        self.alpha = np.ones(n_states)
+        self.mu  = 0
+        self.tau = 3
+        self.scale = 1
 
-        if prior is None:
-            self.alpha = np.ones(n_states)
-            self.mu  = 0
-            self.tau = 3
-            self.scale = 1
-        else:
-            self.alpha = prior['alpha']
-            self.mu = prior['mu']
-            self.tau = prior['tau']
-            self.scale = prior['scale']
+        if type(prior) is dict:
+            self.alpha = np.array(prior.get('alpha', self.alpha))
+            self.mu = prior.get('mu', self.mu)
+            self.tau = prior.get('tau', self.tau)
+            self.scale = prior.get('scale', self.scale)
+
+        if np.sum(self.alpha > 0) != n_states:
+            raise ValueError('number of positive alpha parameters must match number of states')
+
+        if self._is_not_numeric(self.mu) or self._is_not_numeric(self.tau) or self._is_not_numeric(self.scale):
+            raise ValueError('Prior parameters mu, tau, and scale must be a number')
 
         self.n_states = n_states
         self.n_samps = n_samps
@@ -39,12 +41,27 @@ class HMM(object):
         # intialize transition matrix and random starting points
         np.random.seed(self.seed)
 
-        self.transition = np.random.dirichlet(self.alpha, self.n_states)
-        self.mean_samp  = np.zeros((self.n_iters, self.n_states))
-        self.var_samp   = np.zeros((self.n_iters, self.n_states))
+        self.transition = np.random.dirichlet(alpha=self.alpha, size=self.n_states)
+        self.mean_samp  = np.zeros(shape=(self.n_iters, self.n_states))
+        self.var_samp   = np.zeros(shape=(self.n_iters, self.n_states))
 
-        self.mean_samp[0] = np.random.normal(self.mu, self.tau, self.n_states) 
-        self.var_samp[0]  = 1 / np.random.gamma(self.scale, self.scale, self.n_states)
+        self.mean_samp[0] = np.random.normal(
+                                loc=self.mu, 
+                                scale=self.tau, 
+                                size=self.n_states) 
+        self.var_samp[0]  = 1 / np.random.gamma(
+                                shape=self.scale, 
+                                scale=self.scale, 
+                                size=self.n_states)
+
+    def _is_not_numeric(self, x):
+        '''checks if x is not numeric'''
+        return type(x) not in (float, int)
+
+    def _burn(self, x):
+        '''Get rid of burn-in samples'''
+        return x[self.n_burn+1:-1]
+
 
     def _standardize(self, y):
         '''helper function to transform data before sampling'''
@@ -52,85 +69,103 @@ class HMM(object):
         self._y_var = np.var(y)
         return (y - self._y_mean) / self._y_var
 
+
     def _unstandardize(self, y, mean=0, var=1):
         '''helper function to transform data after sampling'''
-
         return (y[self.n_burn:-1] * self._y_var) + self._y_mean
 
-    def _burn(self, y):
-        '''throw away burn in samples'''
-        return y[self.n_burn:-1]
 
+    def _update_backwards(self, y, i):
+        '''updates backwards messsages'''
+        for j in range(len(y)-2, 0, -1):
+            norm = scipy.stats.norm.pdf(
+                        x=y[j+1], 
+                        loc=self.mean_samp[i], 
+                        scale=np.sqrt(self.var_samp[i]))
+            b = np.sum(self._beta[j+1] * self.transition * norm, axis=1)
+            self._beta[j] = b / np.sum(b)
+
+
+    def _update_latent_states(self, y, i):
+        '''samples new latent states'''
+        for j in range(1, len(y)-1):
+            norm = scipy.stats.norm.pdf(
+                        x=y[j], 
+                        loc=self.mean_samp[i], 
+                        scale=np.sqrt(self.var_samp[i]))
+            p = self._beta[j] * norm  * self.transition[self._states[j-1]]
+            self._states[j] = np.random.choice(
+                                range(self.n_states), 
+                                size=1, 
+                                p=p / np.sum(p), 
+                                replace=True)
+
+
+    def _update_emission_params(self, y, i):
+        '''updates parameters of emission distributions'''
+        for h in range(self.n_states):
+                
+            n = np.sum(self._states == h)
+            if n > 0:
+                # update mu
+                tau_n = 1 / (1 / self.tau + n / self.var_samp[i,h])
+                mu_n  = tau_n * ((self.mu / self.tau + n * np.mean(y[self._states == h])) / 
+                        self.var_samp[i,h])
+                self.mean_samp[i+1, h] = np.random.normal(loc=mu_n, scale=np.sqrt(tau_n)) 
+
+                # update varience
+                vn = self.scale + n
+                sn = (1 / vn) * ((self.scale * self.tau + np.sum((y[self._states == h] - 
+                     self.mean_samp[i+1,h])**2)))
+                self.var_samp[i+1,h] = 1 / np.random.gamma(shape=vn, scale=sn, size=1)
+            else:
+                # otherwise sample from prior
+                self.mean_samp[i+1,h]  = np.random.normal(loc=self.mu, scale=np.sqrt(self.tau))
+                self.var_samp[i+1,h] = 1 / np.random.gamma(shape=self.scale, scale=self.scale)
+
+
+    def _update_transitions(self, y):
+        '''updates transition proabilities'''
+        count = np.zeros(shape=(self.n_states, self.n_states))
+        for j in range(1, len(y)-1):
+            count[self._states[j], self._states[j+1]] += 1
+
+        for h in range(self.n_states):
+            self.transition[h] = np.random.dirichlet(alpha=self.alpha + count[h])
+
+
+    def _sample_predict(self, i):
+        '''samples next time point'''
+        self.x_pred[i] = np.random.choice(
+                            range(self.n_states), 
+                            p=self.transition[self._states[-1]])
+        self.y_pred[i] = np.random.normal(
+                            loc=self.mean_samp[i+1, self.x_pred[i]], 
+                            scale=np.sqrt(self.var_samp[i+1, self.x_pred[i]]))
 
     def fit(self, y):
-
         '''
         Preforms inference for HMM using Gibbs sampling and the forward-backward algorithm
-
             y: array, 1-D input data
-
         '''
-
         np.random.seed(self.seed)
 
-        self._beta = np.ones((len(y), self.n_states))
+        self._beta = np.ones(shape=(len(y), self.n_states))
         self._states = np.random.choice(range(self.n_states), size=len(y), replace=True)
-        self.y_pred = np.zeros(self.n_iters)
-        self.x_pred = np.zeros(self.n_iters, dtype=int)
+        self.y_pred = np.zeros(shape=self.n_iters)
+        self.x_pred = np.zeros(shape=self.n_iters, dtype=int)
     
-        # standarize data 
+        # standardize data 
         y = self._standardize(y)
 
         for i in tqdm(range(self.n_iters-1)):
 
-            # update backwards messages
-            for j in range(len(y)-2, 0, -1):
-                b = np.sum(
-                        self._beta[j+1] * 
-                        self.transition * 
-                        scipy.stats.norm.pdf(y[j+1], self.mean_samp[i], np.sqrt(self.var_samp[i])),
-                    axis=1)
-                self._beta[j] = b / np.sum(b)
+            self._update_backwards(y, i) 
+            self._update_latent_states(y, i)
+            self._update_emission_params(y, i)
+            self._update_transitions(y)
 
-            # sample latent states
-            for k in range(1, len(y)-1):
-                p = self._beta[k] * \
-                        scipy.stats.norm.pdf(y[k], self.mean_samp[i], np.sqrt(self.var_samp[i])) * \
-                        self.transition[self._states[k-1]]
-                self._states[k] = np.random.choice(range(self.n_states), size=1, p=p/np.sum(p), replace=True)
-
-
-            # sample emission distributions
-            for h in range(self.n_states):
-                
-                n = np.sum(self._states == h)
-                if n > 0:
-                    # update mu
-                    tau_n = 1 / (1 / self.tau + n / self.var_samp[i,h])
-                    mu_n  = tau_n * (self.mu / self.tau + n * np.mean(y[self._states == h])) / self.var_samp[i,h]
-                    self.mean_samp[i+1, h] = np.random.normal(mu_n, np.sqrt(tau_n)) 
-
-                    # update varience
-                    vn = self.scale + n
-                    sn = (1 / vn) * (self.scale * self.tau + np.sum((y[self._states == h] - self.mean_samp[i+1,h])**2))
-                    self.var_samp[i+1,h] = 1 / np.random.gamma(vn, sn, 1)
-
-                else:
-                    # otherwise sample from prior
-                    self.mean_samp[i+1,h]  = np.random.normal(self.mu, np.sqrt(self.tau))
-                    self.var_samp[i+1,h] = 1 / np.random.gamma(self.scale, self.scale)
-
-            # sample transition probabilities
-            count = np.zeros((self.n_states, self.n_states))
-            for k in range(1, len(y)-1):
-                count[self._states[k], self._states[k+1]] += 1
-
-            for h in range(self.n_states):
-                self.transition[h] = np.random.dirichlet(self.alpha + count[h])
-
-            # predict next time point
-            self.x_pred[i] = np.random.choice(range(self.n_states), p=self.transition[self._states[-1]])
-            self.y_pred[i] = np.random.normal(self.mean_samp[i+1, self.x_pred[i]], np.sqrt(self.var_samp[i+1, self.x_pred[i]]))
+            self._sample_predict(i)
         
         # throw away burn-in samples and unstandarize
         self.y_pred = self._burn(self._unstandardize(self.y_pred))
--- bayesHMM/unittest.py
+++ bayesHMM/unittest.py
@@ -1,12 +1,81 @@
 import unittest
-from hmm import HMM
+import numpy as np
+from unittest import TestCase
+from bayesHMM import HMM
 
-class HmmInitTest(TestCase):
+NSAMP  = 1000
+NBURN  = 500
+NSTATES = 3
+SEED = 42
 
-    def test_load(self):
-        self.assertRaises(Exception, HMM())
+class hmmInitTests(TestCase):
+        
+    def test_load_params(self):
+        '''testing loading well formed parameters'''
+        PRIOR = {
+            'alpha': [1, 3, 1],
+            'mu': 1.1,
+            'tau': 2,
+            'scale': 2.1
+        } 
+        hmm = HMM(NSTATES, NSAMP, NBURN, prior=PRIOR, seed=SEED)
+        self.assertTrue(hmm.n_states == NSTATES)
+        self.assertTrue(hmm.n_samps == NSAMP)
+        self.assertTrue(hmm.n_burn == NBURN)
+        self.assertTrue(hmm.seed == SEED)
+        
+        self.assertTrue(np.all(hmm.alpha == PRIOR['alpha']))
+        self.assertTrue(hmm.mu == PRIOR['mu'])
+        self.assertTrue(hmm.tau == PRIOR['tau'])
+        self.assertTrue(hmm.scale == PRIOR['scale'])
+
+    def test_partial_load(self):
+        '''testing partial loading of prior parameters'''
+        PRIOR = {
+            'tau': 2,
+            'scale': 4
+        }
+
+        hmm = HMM(NSTATES, NSAMP, prior=PRIOR)
+        self.assertTrue(hmm.tau == PRIOR['tau'])
+        self.assertTrue(hmm.scale == PRIOR['scale'])
+        self.assertTrue(hmm.mu == 0)
+        self.assertTrue(np.all(hmm.alpha == [1, 1, 1]))
+
+
+    def test_bad_alpha(self):
+        '''testing malformed variations of alpha'''
+        BAD_PRIOR1 = {'alpha': [3, -1, 2]}
+        BAD_PRIOR2 = {'alpha': [1, 2, 3, 4]}
+        BAD_PRIOR3 = {'alpha': [0, 1, 2, 3]}
+
+        with self.assertRaises(ValueError):
+            HMM(NSTATES, NSAMP, prior=BAD_PRIOR1)
+            HMM(NSTATES, NSAMP, prior=BAD_PRIOR2)
+            HMM(NSTATES, NSAMP, prior=BAD_PRIOR3)
+
+    def test_bad_prior(self):
+        '''testing malformed variations of mu, tau, and scale'''
+        BAD_PRIOR1 = {'mu': [1, 2, 3]}
+        BAD_PRIOR2 = {'tau': -2}
+        BAD_PRIOR3 = {'tau': [1, 2, 3]}
+        BAD_PRIOR4 = {'scale': -1}
+        BAD_PRIOR5 = {'scale': [1, 2, 3]}
+        with self.assertRaises(ValueError):
+            HMM(NSTATES, NSAMP, prior=BAD_PRIOR1)
+            HMM(NSTATES, NSAMP, prior=BAD_PRIOR2)
+            HMM(NSTATES, NSAMP, prior=BAD_PRIOR3)
+            HMM(NSTATES, NSAMP, prior=BAD_PRIOR4)
+            HMM(NSTATES, NSAMP, prior=BAD_PRIOR5)
+
+    def test_init(self):
+        '''test intialization of data storage'''
+        hmm = HMM(NSTATES, NSAMP, NBURN, seed=SEED)
+        self.assertTrue(hmm.transition.shape == (NSTATES, NSTATES))
+        self.assertTrue(np.all(hmm.mean_samp[0] != 0))
+        self.assertTrue(np.all(hmm.var_samp[0] != 0))
+        self.assertTrue(hmm.mean_samp.shape == (NSAMP+NBURN+1, NSTATES))
+        self.assertTrue(hmm.var_samp.shape == (NSAMP+NBURN+1, NSTATES))
 
 
 
-if __name__ == '__main__':
-    unittest.main()
